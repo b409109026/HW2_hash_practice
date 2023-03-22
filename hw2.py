518db729c3f3a051b0b8926b78f6e395327024c6
b409109026 <126319693+b409109026@users.noreply.github.com>
2023-03-23 01:56:18 +0800

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -2,7 +2,25 @@
 # coding: utf-8
 
 # In[ ]:
+#建立字典
+word_count = {}
 
+#讀取文字檔
+with open('hw2_data.txt', 'r') as f:
+    for line in f:
+        #將每行內容轉換成單字
+        word = line.strip()
+        #如果單字已經出現過就加一
+        if word in word_count:
+            word_count[word] += 1
+        #如果單字沒出現過就算一    
+        else:
+            word_count[word] = 1
+
+#輸出結果
+print("總共有 {} 個不重複的英文字".format(len(word_count)))
+for word, count in word_count.items():
+    print("{} 出現了 {} 次".format(word, count))
 
 
 
